818e6e79df08aa40a8be90c7ba6c2421bed014ab
Edward Arcuri <earcuri@paloaltonetworks.com>
2017-11-20 09:18:07 -0700

    Modify startup procedures for background processes

--- project/__init__.py
+++ project/__init__.py
@@ -49,55 +49,55 @@ def create_app():
                          'port': app.config["ELASTICSEARCH_PORT"]}])
 
     
-    def startProcessing():
-        '''
-        Initializes the search list and sets the "processed" value to 17 so that
-        the app knows that the document has been picked up for processing. It
-        then calls the threading/multiprocessing function processDocuments() to
-        continually search through ElasticSearch DB for unprocessed docs.
-        '''
-        # Set up variables used for processing
-        docIds = {}
-        indexes = ("sfn-dns","sfn-iot")
-        qSize = app.config["INIT_QUERY_SIZE"]
+    # def startProcessing():
+    #     '''
+    #     Initializes the search list and sets the "processed" value to 17 so that
+    #     the app knows that the document has been picked up for processing. It
+    #     then calls the threading/multiprocessing function processDocuments() to
+    #     continually search through ElasticSearch DB for unprocessed docs.
+    #     '''
+    #     # Set up variables used for processing
+    #     docIds = {}
+    #     indexes = ("sfn-dns","sfn-iot")
+    #     qSize = app.config["INIT_QUERY_SIZE"]
 
-        try:
-            for index in indexes:
-                # Search for all docs that have processed set to 0, which means they 
-                # have not been processed yet.
-                docs = es.search(index=index,body={
-                                                    "size": qSize, 
-                                                    "query": {
-                                                        "match_all": {}
-                                                    },
-                                                    "sort": [
-                                                        {
-                                                        "msg_gen_time": {
-                                                            "order": "desc"
-                                                        }
-                                                        }
-                                                    ]
-                                                }
-                )
-                app.logger.info(
-                            "Found {0} unpropcessed document(s) for {1}"
-                                        .format(docs['hits']['total'],index))
+    #     try:
+    #         for index in indexes:
+    #             # Search for all docs that have processed set to 0, which means they 
+    #             # have not been processed yet.
+    #             docs = es.search(index=index,body={
+    #                                                 "size": qSize, 
+    #                                                 "query": {
+    #                                                     "match_all": {}
+    #                                                 },
+    #                                                 "sort": [
+    #                                                     {
+    #                                                     "msg_gen_time": {
+    #                                                         "order": "desc"
+    #                                                     }
+    #                                                     }
+    #                                                 ]
+    #                                             }
+    #             )
+    #             app.logger.info(
+    #                         "Found {0} unpropcessed document(s) for {1}"
+    #                                     .format(docs['hits']['total'],index))
 
-                for doc in docs['hits']['hits']:
-                    docKey = doc['_id']
-                    print("{0}".format(docKey))
-                    docIds[docKey] = index
+    #             for doc in docs['hits']['hits']:
+    #                 docKey = doc['_id']
+    #                 print("{0}".format(docKey))
+    #                 docIds[docKey] = index
                 
                 
-        except TransportError:
-            app.logger.warning('Initialization was unable to find the index {0}'.format(index))
+    #     except TransportError:
+    #         app.logger.warning('Initialization was unable to find the index {0}'.format(index))
         
-        return docIds
+    #     return docIds
 
-    # Initiate
-    docIds = startProcessing()
-    pprint.pformat(json.dumps(docIds))
-    app.logger.debug("Found {0}".format(docIds))
+    # # Initiate
+    # docIds = startProcessing()
+    # pprint.pformat(json.dumps(docIds))
+    # app.logger.debug("Found {0}".format(docIds))
 
     # When we kill Flask (SIGTERM), we want to clear the trigger for the
     # next thread
