764e0085d00a5abfc1e92e1843bfcf4d9c8974b6
Edward Arcuri <earcuri@paloaltonetworks.com>
2017-11-22 17:19:07 -0700

    add searches based on doc IDs for sfn-dns

--- project/api/utils.py
+++ project/api/utils.py
@@ -2,10 +2,14 @@ from elasticsearch import TransportError
 
 
 def processDNS(es,app):
-
+    '''
+    This funciton is used to add AutoFocus information to the unprocessed 
+    entries in ElasticSearch.  
+    '''
     qSize = app.config["DNS_INIT_QUERY_SIZE"]
 
     try:
+        # Query for the unprocessed DNS entries.  
         docs = es.search(index="sfn-dns",
                          body={"size": qSize, 
                                "query": { 
@@ -22,23 +26,22 @@ def processDNS(es,app):
         app.logger.info("Found {0} unpropcessed document(s) for {1}"
                                 .format(docs['hits']['total'],"sfn-dns"))
 
-        #docList = {doc['_id']:doc['threat_id'] for doc in docs['hits']['hits']}
+        # Create a dictionary of the docs and the associate threat_id
+       # docIds = {doc['_id']:doc['_source']['threat_id'] for doc in }
+        #print(dict(docIds))
+        
+        # Need to manipulate the threat_id to break out the domain - would be
+        # better to do this with the logger - need to figure that out.
         docIds = dict()
         for entry in docs['hits']['hits']:
             docKey = entry['_id']
-            print(docKey)
             docIds[docKey] = entry['_source']['threat_id']
             print("{0} : {1}".format(docKey,docIds[docKey]))
             
     except TransportError:
         app.logger.warning('Initialization was unable to find the index sfn-dns')
     
-#     return docIds
 
-# # Initiate
-# docIds = startProcessing()
-# pprint.pformat(json.dumps(docIds))
-# app.logger.debug("Found {0}".format(docIds))
 
 def main():
     pass
