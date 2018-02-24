83b129ef7e0dc4d92ba8b626bf2a15478a89c398
Edward Arcuri <sdndude@users.noreply.github.com>
2018-02-24 08:25:34 -0700

    Update __init__.py

--- project/__init__.py
+++ project/__init__.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 # ---------- APPLICATION SETTINGS --------------
 #
 # Current version number of SafeNetworking
-app.config['VERSION'] = "2.0.2"
+app.config['VERSION'] = "2.0.3"
 #
 # When set to True, this slows down the logging by only processing 1 event at a
 # time and allows us to see what is going on if there are bugs
